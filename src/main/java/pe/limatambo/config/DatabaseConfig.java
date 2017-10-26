c3e09cbb79e577a6e9008ae3cd359b590223b81a
MarioMario <marioramos083@gmail.com>
2017-10-26 00:00:00 -0500

    ooo

--- src/main/java/pe/limatambo/config/DatabaseConfig.java
+++ src/main/java/pe/limatambo/config/DatabaseConfig.java
@@ -21,51 +21,53 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @Configuration
 @EnableTransactionManagement
-@PropertySource(value = {"file:/opt/aplicaciones/limatambo/limatambo.properties"})
+//@PropertySource(value = {"file:/opt/aplicaciones/limatambo/limatambo.properties"})
 public class DatabaseConfig {
     
     @Autowired
     private Environment environment;
+    @Autowired
+    DataSource dataSource;
     private final Logger loggerConfig = LoggerFactory.getLogger(getClass());
     
     @Bean
     public LocalSessionFactoryBean sessionFactory() throws PropertyVetoException{
         try {
             LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-            sessionFactory.setDataSource(dataSource());
+            sessionFactory.setDataSource(dataSource);
             sessionFactory.setPackagesToScan(environment.getRequiredProperty("entitymanager.packagesToScan"));//(new String[]{"pe.cajapaita.backerp.contabilidad.entidad"});
-            sessionFactory.setHibernateProperties(hibernateProperties());
+//            sessionFactory.setHibernateProperties(hibernateProperties());
             return sessionFactory;
-        } catch (PropertyVetoException | IllegalStateException e) {
+        } catch (IllegalStateException e) {
             loggerConfig.error(e.getMessage());
             throw e;
         }
     }
     
-    @Bean
-    public DataSource dataSource() throws PropertyVetoException{          
-        ComboPooledDataSource dataSource = new ComboPooledDataSource();
-        dataSource.setDriverClass(environment.getRequiredProperty("db.driver"));
-        dataSource.setJdbcUrl(environment.getRequiredProperty("db.url"));
-        dataSource.setUser(environment.getRequiredProperty("db.username"));
-        dataSource.setPassword(environment.getRequiredProperty("db.password"));
-        dataSource.setAcquireIncrement(Integer.parseInt(environment.getRequiredProperty("dataSource.acquireIncrementcquireIncrement")));
-        dataSource.setMinPoolSize(Integer.parseInt(environment.getRequiredProperty("dataSource.minPoolSize")));
-        dataSource.setMaxPoolSize(Integer.parseInt(environment.getRequiredProperty("dataSource.maxPoolSize")));
-        dataSource.setMaxIdleTime(Integer.parseInt(environment.getRequiredProperty("dataSource.maxIdleTime")));
-        dataSource.setCheckoutTimeout(Integer.parseInt(environment.getRequiredProperty("dataSource.checkoutTimeout")));
-        dataSource.setNumHelperThreads(Integer.parseInt(environment.getRequiredProperty("dataSource.mumHelperThreads")));
-        dataSource.setMaxStatements(Integer.parseInt(environment.getRequiredProperty("dataSource.maxStatements")));
-        dataSource.setUnreturnedConnectionTimeout(Integer.parseInt(environment.getRequiredProperty("dataSource.unreturnedConnectionTimeout")));
-        return dataSource;
-    }
-    
-    private Properties hibernateProperties() {
-        Properties properties = new Properties();
-        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
-        return properties;
-    }
-    
+//    @Bean
+//    public DataSource dataSource() throws PropertyVetoException{          
+//        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+//        dataSource.setDriverClass(environment.getRequiredProperty("db.driver"));
+//        dataSource.setJdbcUrl(environment.getRequiredProperty("db.url"));
+//        dataSource.setUser(environment.getRequiredProperty("db.username"));
+//        dataSource.setPassword(environment.getRequiredProperty("db.password"));
+//        dataSource.setAcquireIncrement(Integer.parseInt(environment.getRequiredProperty("dataSource.acquireIncrementcquireIncrement")));
+//        dataSource.setMinPoolSize(Integer.parseInt(environment.getRequiredProperty("dataSource.minPoolSize")));
+//        dataSource.setMaxPoolSize(Integer.parseInt(environment.getRequiredProperty("dataSource.maxPoolSize")));
+//        dataSource.setMaxIdleTime(Integer.parseInt(environment.getRequiredProperty("dataSource.maxIdleTime")));
+//        dataSource.setCheckoutTimeout(Integer.parseInt(environment.getRequiredProperty("dataSource.checkoutTimeout")));
+//        dataSource.setNumHelperThreads(Integer.parseInt(environment.getRequiredProperty("dataSource.mumHelperThreads")));
+//        dataSource.setMaxStatements(Integer.parseInt(environment.getRequiredProperty("dataSource.maxStatements")));
+//        dataSource.setUnreturnedConnectionTimeout(Integer.parseInt(environment.getRequiredProperty("dataSource.unreturnedConnectionTimeout")));
+//        return dataSource;
+//    }
+//    
+//    private Properties hibernateProperties() {
+//        Properties properties = new Properties();
+//        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+//        return properties;
+//    }
+//    
     @Bean
     @Autowired
     public HibernateTransactionManager transactionManager(SessionFactory s) {
