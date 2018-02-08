78cc07ec5b17b95f501f9b22885238a47285539a
MarioMario <marioramos083@gmail.com>
2018-02-08 00:20:10 -0500

    fechas

--- src/main/java/pe/limatambo/servicio/impl/PedidoServicioImp.java
+++ src/main/java/pe/limatambo/servicio/impl/PedidoServicioImp.java
@@ -129,7 +129,7 @@ public class PedidoServicioImp extends GenericoServicioImpl<Pedido, Integer> imp
         filtro.setProjection(Projections.projectionList()
                 .add(Projections.property("id"), "codigo")
                 .add(Projections.property("descripcion"), "descripcion")
-                .add(Projections.property("fechalimite"), "fechaPedido")
+                .add(Projections.property("fechapedido"), "fechaPedido")
                 .add(Projections.property("fechaentrega"), "fechaEntrega")
                 .add(Projections.property("direccion"), "direccion")
                 .add(Projections.property("usuariosave"), "usuariosave")
